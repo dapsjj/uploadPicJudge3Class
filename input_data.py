0d70f65ef93e851ec10f2e057cb213aaa2a8f64e
dapsjj <dapsjj@qq.com>
2017-08-15 07:47:53 +0800

    Create input_data.py

---
+++ input_data.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+train_dir = r'D:/workspace/uploadPicJudge3Class/train/'
+
+def get_files(file_dir):
+    breads = []
+    hambergers  = []
+    hotdogs  = []
+    label_breads = []
+    label_hambergers = []
+    label_hotdogs = []
+    for file in os.listdir(file_dir):
+        name = file.split(sep='.')
+        if name[0] == 'bread':
+            breads.append(file_dir + file)
+            label_breads.append(0)
+        elif name[0] == 'hot':
+            hotdogs.append(file_dir + file)
+            label_hotdogs.append(1)
+        else :
+            hambergers.append(file_dir + file)
+            label_hambergers.append(2)
+    print('There are %d breads\nThere are %d hotdogs\nThere are %d hambergers' % (len(breads), len(hotdogs),len(hambergers)))
+    image_list = np.hstack((breads, hotdogs,hambergers))
+    label_list = np.hstack((label_breads, label_hotdogs,label_hambergers))
+    temp = np.array([image_list, label_list])
+    temp = temp.transpose()
+    np.random.shuffle(temp)
+    image_list = list(temp[:, 0])
+    label_list = list(temp[:, 1])
+    label_list = [int(i) for i in label_list]
+    return image_list, label_list
+
+def get_batch(image, label, image_W, image_H, batch_size, capacity):
+    image = tf.cast(image, tf.string)
+    label = tf.cast(label, tf.int32)
+    input_queue = tf.train.slice_input_producer([image, label])
+    label = input_queue[1]
+    image_contents = tf.read_file(input_queue[0])
+    image = tf.image.decode_jpeg(image_contents, channels=3)
+    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
+    image = tf.image.per_image_standardization(image)
+    image_batch, label_batch = tf.train.batch([image, label],
+                                              batch_size=batch_size,
+                                              num_threads=64,
+                                              capacity=capacity)
+
+    label_batch = tf.reshape(label_batch, [batch_size])
+    image_batch = tf.cast(image_batch, tf.float32)
+
+    return image_batch, label_batch
+
+BATCH_SIZE = 2
+CAPACITY = 256
+IMG_W = 208
+IMG_H = 208
+
+train_dir = r'D:/workspace/uploadPicJudge3Class/train/'
+image_list, label_list = get_files(train_dir)
+image_batch, label_batch = get_batch(image_list, label_list, IMG_W, IMG_H, BATCH_SIZE, CAPACITY)
+
+with tf.Session() as sess:
+    i = 0
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    try:
+        while not coord.should_stop() and i < 1:
+            img, label = sess.run([image_batch, label_batch])
+            for j in np.arange(BATCH_SIZE):
+                # print('label: %d' % label[j])
+                pass
+            i += 1
+    except tf.errors.OutOfRangeError:
+        print('done!')
+    finally:
+        coord.request_stop()
+    coord.join(threads)
+
+
+
+
+
+
+
